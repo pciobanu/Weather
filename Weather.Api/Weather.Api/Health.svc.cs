3cf9985642e6afcb9cd971baf56299689cacef15
pciobanu <dinopeter2002@yahoo.com>
2013-04-22 21:44:02 +0300

    NewVersion

---
+++ Weather.Api/Weather.Api/Health.svc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Weather.Api
+{
+     [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Health : IHealth
+    {
+
+        #region IHealth Members
+       
+        public string GetHealth()
+        {
+            return ("REST SERVICE IS UP  AND  RUNNING");
+        }
+
+
+     
+
+     
+
+        #endregion
+    }
+}
---
+++ Weather.Api/Weather.Api/IHealth.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Weather.Api
+{
+   
+    [ServiceContract]
+    public interface IHealth
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET",UriTemplate = "health")]
+        string GetHealth();
+    }
+
+
+
+
+}
